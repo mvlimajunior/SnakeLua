9c5b0492347c29155114dcb9c30ed4c3f72eac0d
LosLobos <igormorse@hotmail.com>
2017-05-09 02:23:39 -0300

    Atualização do Jogo
    - Criação de Blocos da Maneira Correta.
    
    - Adição de Comida no Jogo com aleatoriadade.
    
    - Organização do Código e nomes.

--- main.lua
+++ main.lua
@@ -1,48 +1,89 @@
 screenWidth = love.graphics.getWidth()
 screenHeight = love.graphics.getHeight()
 
-block_body_size = 20
-body_speed_default = 100
-body_speed_default_stop = 0
+default_block_size = 20
 
-linetable = {10,10,10,screenHeight-10,screenWidth-10,screenHeight-10,screenWidth-10,10,10,10}
+player_movement_speed = 100
+player_no_speed = 0
 
 function love.load ()
+
+  -- Inicializa a Cor do Cenário
   love.graphics.setBackgroundColor(255,255,255)
+
+  -- Inicializa a Cor das Linhas de Demarcação do Cenário.
   love.graphics.setColor(0,0, 0)
 
-  player = { x = screenWidth/2 , y = screenHeight/2,size_body = 0 ,speed_x = body_speed_default,speed_y = body_speed_default_stop,}
-  body = {}
+  scenarioLimits = {10,10,10,screenHeight-10,screenWidth-10,screenHeight-10,screenWidth-10,10,10,10}
 
-  -- table.insert(player,body)
+  -- Iniciliza o Jogador.
+  player = {
+    x = screenWidth/2,
+    y = screenHeight/2,
+    speed_x = player_movement_speed,
+    speed_y = player_no_speed,
+    body_size = 0,
+    body = {}
+  }
 
-  add_block()
-  add_block()
+  food = {
+    x = nil,
+    y = nil,
+    isAlive = true
+  }
+
+  -- Inicializa dois blocos ao Jogador e Inicializa comida no cenário.
+  playerAddBlock()
+  playerAddBlock()
+  respawnPlayerFood()
 
 end
 
+-- Aumenta o comprimento do Jogador.
+function playerAddBlock()
 
-function add_block()
-    new_block = {x = player.x + block_body_size, y = player.y + block_body_size}
-    table.insert(body,new_block)
-    player.size_body = player.size_body + 1
-    print("Criei Corpo no Player! : ")
-    print(player.size_body)
+  if (player.body_size == 0) then
+    new_block = {
+      x = player.x,
+      y = player.y + default_block_size
+    }
+  else
+    new_block = {
+      x = player.x,
+      y = player.y + ( default_block_size * (player.body_size + 1))
+    }
+
+  end
+
+  table.insert(player.body,new_block)
+
+  player.body_size = player.body_size + 1
+
+  print("Criei Corpo no Player! : ")
+  print(player.body_size)
+end
+
+function respawnPlayerFood()
+
+  food.x = love.math.random(10, screenWidth - 10)
+  food.y = love.math.random(10, screenHeight - 10)
+  food.isAlive = true
+
 end
 
 function love.keypressed (key)
     if key == 'left' then
-        player.speed_x = -body_speed_default
-        player.speed_y = body_speed_default_stop
+        player.speed_x = -player_movement_speed
+        player.speed_y = player_no_speed
     elseif key == 'right' then
-        player.speed_x = body_speed_default
-        player.speed_y = body_speed_default_stop
+        player.speed_x = player_movement_speed
+        player.speed_y = player_no_speed
     elseif key == 'up' then
-        player.speed_x = body_speed_default_stop
-        player.speed_y = -body_speed_default
+        player.speed_x = player_no_speed
+        player.speed_y = -player_movement_speed
     elseif key == 'down' then
-        player.speed_y = body_speed_default
-        player.speed_x = body_speed_default_stop
+        player.speed_y = player_movement_speed
+        player.speed_x = player_no_speed
     end
 end
 
@@ -54,18 +95,37 @@ end
   --]]
 
 function love.update (dt)
-  player.x =  player.x + player.speed_x * dt
-  player.y =  player.y + player.speed_y * dt
+  --player.x =  player.x + player.speed_x * dt
+  --player.y =  player.y + player.speed_y * dt
 end
 
+function drawPlayer()
+
+  love.graphics.setColor(255, 0, 0, 180)
+
+  -- Desenho do Jogador. ( Cabeça )
+  love.graphics.rectangle( "fill", player.x, player.y, default_block_size, default_block_size )
+
+  love.graphics.setColor(0, 0, 0, 255)
+  -- Desenho do Corpo.
+ for i,block in ipairs(player.body) do
+   love.graphics.rectangle( "fill", block.x, block.y, default_block_size, default_block_size )
+ end
+
+end
 
 function love.draw()
-  -- quad start
-   love.graphics.line(linetable)
 
-   love.graphics.rectangle( "fill", player.x, player.y, block_body_size, block_body_size )
+  -- Desenho do Cenário.
+  love.graphics.line(scenarioLimits)
 
-   for i,block in ipairs(body) do
-     love.graphics.rectangle( "fill", body[i].x, body[i].y, block_body_size, block_body_size )
-   end
+  -- Desenha o Jogador.
+  drawPlayer()
+
+  -- Desenho da Comida.
+  if (food.isAlive) then
+    love.graphics.setColor(0,0,255)
+    love.graphics.rectangle( "fill", food.x, food.y, default_block_size, default_block_size )
+  end
+
 end
